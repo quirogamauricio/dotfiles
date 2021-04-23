f4cacd6b4d61bf57bf225f765f419b039f8f3cd2
Mauricio Quiroga <mauricioq17@gmail.com>
2021-04-23 18:18:00 -0300

    Add script to start Firefox using my personal profile and an env. variable to set that browser as the default one

--- bin/env-vars.sh
+++ bin/env-vars.sh
@@ -2,3 +2,5 @@ export FZF_DEFAULT_COMMAND='fd --type f --hidden --exclude .git --exclude node_m
 export FZF_DEFAULT_OPTS="--ansi"
 
 export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/docker.sock
+
+export BROWSER=/usr/bin/firefox
